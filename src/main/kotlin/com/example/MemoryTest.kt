3b15bf91ea326919773a8e661efff8c2fba7ca62
Igor Shchipanov <oneyearday@gmail.com>
2024-08-12 16:06:42 +0300

    tests

--- src/main/kotlin/com/example/MemoryTest.kt
+++ src/main/kotlin/com/example/MemoryTest.kt
@@ -13,7 +13,17 @@ fun main() = runBlocking {
             delay(1)
         }
         launch {
+            delay(5000)
             print(".")
         }
+    }
+}
+
+fun main2() {
+    for (i in 1..50_000) {
+        Thread() {
+            Thread.sleep(5000)
+            print(".")
+        }.start()
     }
 }
